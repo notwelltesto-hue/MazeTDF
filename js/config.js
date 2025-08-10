c34cd1b37605797949164721643f8dd3c17d75b7
notwelltesto-hue <notwelltesto@gmail.com>
2025-08-10 10:26:37 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,9 +6,10 @@ export let CANVAS_W = window.innerWidth - 30;
 export let CANVAS_H = window.innerHeight - 150;
 
 export const spawnIntervalMs = 1700;
+export const BUILD_TIME_SECONDS = 3.0; // How long it takes to build a tower
 
-export const COST = { basic: 20, lighter: 30, mine: 50 };
-export const TOWER = { BASIC: 'basic', LIGHTER: 'lighter', MINE: 'mine' };
+export const COST = { basic: 20, lighter: 30, mine: 50, supply: 40 };
+export const TOWER = { BASIC: 'basic', LIGHTER: 'lighter', MINE: 'mine', SUPPLY: 'supply' };
 
 export function updateCanvasSize() {
     CANVAS_W = window.innerWidth - 30;
